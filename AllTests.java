53db8651ba6069d506f5119ce3a4f8d634d32746
Craig Parker <craig@craigcode.com>
2017-05-09 13:40:42 -0500

    combined 2 existing tests into 1 test suite

---
+++ AllTests.java
@@ -0,0 +1,11 @@
+package com.craigcode.basic_experiment_with_unit_testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AutomobileAgeTest.class, AutomobileTopSpeedConversionTest.class })
+public class AllTests {
+
+}
